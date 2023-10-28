703b328c8925f4dcb99487c251802e2d0fa996eb
Pavel Derendyaev <dddpaul@gmail.com>
2023-10-28 20:56:37 +0300

    Add stats to tmux

--- tmux.sh
+++ tmux.sh
@@ -16,40 +16,109 @@ fi
 
 tmux new-session -d -s $session
 
-# 6 horizontal panes and low command pane
+# 1st divided horizontal pane
 tmux split-window -v
-tmux resize-pane -U 10
-tmux split-window -v
-tmux resize-pane -U 10
-tmux split-window -v
-tmux resize-pane -U 10
-tmux split-window -v
-tmux resize-pane -U 10
-tmux split-window -v
-tmux resize-pane -U 10
-tmux split-window -v
-tmux resize-pane -U 10
+tmux select-pane -U
+tmux split-window -h
+tmux resize-pane -R 40
+tmux resize-pane -U 24
+tmux select-pane -D
 
-# Run 6 workers
+# 2nd divided horizontal pane
+tmux split-window -v
 tmux select-pane -U
-tmux send-keys -t $session 'make worker-bar' Enter
+tmux split-window -h
+tmux resize-pane -R 40
+tmux resize-pane -U 20
+tmux select-pane -D
+
+# 3rd divided horizontal pane
+tmux split-window -v
 tmux select-pane -U
-tmux send-keys -t $session 'make worker-bar' Enter
+tmux split-window -h
+tmux resize-pane -R 40
+tmux resize-pane -U 16
+tmux select-pane -D
+
+# 4th divided horizontal pane
+tmux split-window -v
 tmux select-pane -U
-tmux send-keys -t $session 'make worker-bar' Enter
+tmux split-window -h
+tmux resize-pane -R 40
+tmux resize-pane -U 12
+tmux select-pane -D
+
+# 5th divided horizontal pane
+tmux split-window -v
 tmux select-pane -U
-tmux send-keys -t $session 'make worker-bar' Enter
+tmux split-window -h
+tmux resize-pane -R 40
+tmux resize-pane -U 8
+tmux select-pane -D
+
+# 6th divided horizontal pane
+tmux split-window -v
 tmux select-pane -U
-tmux send-keys -t $session 'make worker-bar' Enter
+tmux split-window -h
+tmux resize-pane -R 40
+tmux resize-pane -U 4
+tmux select-pane -D
+
+# Run 6th worker and display stats
 tmux select-pane -U
-tmux send-keys -t $session 'make worker-bar' Enter
+tmux select-pane -L
+tmux send-keys -t $session 'SERVER_PORT=8006 make worker-bar' Enter
+tmux select-pane -R
+tmux send-keys -t $session 'watch -n1 "curl -sS localhost:8006/actuator/stats | jq ."' Enter
+
+# Run 5th worker and display stats
+tmux select-pane -U
+tmux select-pane -L
+tmux send-keys -t $session 'SERVER_PORT=8005 make worker-bar' Enter
+tmux select-pane -R
+tmux send-keys -t $session 'watch -n1 "curl -sS localhost:8005/actuator/stats | jq ."' Enter
+
+# Run 4th worker and display stats
+tmux select-pane -U
+tmux select-pane -L
+tmux send-keys -t $session 'SERVER_PORT=8004 make worker-bar' Enter
+tmux select-pane -R
+tmux send-keys -t $session 'watch -n1 "curl -sS localhost:8004/actuator/stats | jq ."' Enter
+
+# Run 3th worker and display stats
+tmux select-pane -U
+tmux select-pane -L
+tmux send-keys -t $session 'SERVER_PORT=8003 make worker-bar' Enter
+tmux select-pane -R
+tmux send-keys -t $session 'watch -n1 "curl -sS localhost:8003/actuator/stats | jq ."' Enter
+
+# Run 2th worker and display stats
+tmux select-pane -U
+tmux select-pane -L
+tmux send-keys -t $session 'SERVER_PORT=8002 make worker-bar' Enter
+tmux select-pane -R
+tmux send-keys -t $session 'watch -n1 "curl -sS localhost:8002/actuator/stats | jq ."' Enter
+
+# Run 1th worker and display stats
+tmux select-pane -U
+tmux select-pane -L
+tmux send-keys -t $session 'SERVER_PORT=8001 make worker-bar' Enter
+tmux select-pane -R
+tmux send-keys -t $session 'watch -n1 "curl -sS localhost:8001/actuator/stats | jq ."' Enter
 
 # Return to command pane
-tmux select-pane -L
-tmux select-pane -L
-tmux select-pane -L
-tmux select-pane -L
-tmux select-pane -L
+tmux select-pane -D
+tmux select-pane -D
+tmux select-pane -D
+tmux select-pane -D
+tmux select-pane -D
+tmux select-pane -D
+
+# Run stats and prepare for starter command
+tmux split-window -h
+tmux resize-pane -R 40
+tmux select-pane -R
+tmux send-keys -t $session 'watch -n1 "curl -sS localhost:8080/actuator/stats | jq ."' Enter
 tmux select-pane -L
 
 attach
